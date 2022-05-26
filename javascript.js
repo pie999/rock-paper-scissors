c5d7329b9bf414df2cf742778bd868707afb8431
pie999 <pietro.vergara@libero.it>
2022-05-26 16:03:11 +0200

    Make computerPlay function

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function computerPlay(){
+    let choice = Math.floor(Math.random()*3);
+    if (choice == 0) return "Rock";
+    else if (choice == 1) return "Paper";
+    else return "Scissors";
+}
+console.log(computerPlay());
