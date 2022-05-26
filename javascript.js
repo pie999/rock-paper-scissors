43b1a1963bdd39fb2379e1adab711557ce1e5fa7
pie999 <pietro.vergara@libero.it>
2022-05-26 16:30:16 +0200

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,35 @@
 function computerPlay(){
     let choice = Math.floor(Math.random()*3);
-    if (choice == 0) return "Rock";
-    else if (choice == 1) return "Paper";
-    else return "Scissors";
+    if (choice == 0) return "rock";
+    else if (choice == 1) return "paper";
+    else return "scissors";
 }
-console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == "rock"){
+        if (computerSelection == "scissors")
+            return "You win! Rock beats Scissors";
+        else if (computerSelection == "paper")
+            return "You lose! Paper beats Rock";
+        else return "Tie! Both players chose Rock"
+    }
+    if (playerSelection == "paper"){
+        if (computerSelection == "rock")
+            return "You win! paper beats rock";
+        else if (computerSelection == "scissors")
+            return "You lose! scissor beats paper";
+        else return "Tie! Both players chose paper"
+    }
+    if (playerSelection == "scissors"){
+        if (computerSelection == "paper")
+            return "You win! scissor beats paper";
+        else if (computerSelection == "rock")
+            return "You lose! rock beats scissors";
+        else return "Tie! Both players chose scissors"
+    }
+}
+
+const playerSelection = "SCISSORS";
+const computerSelection = computerPlay();
+console.log(computerSelection);
+console.log(playRound(playerSelection.toUpperCase().toLowerCase(), computerSelection));
